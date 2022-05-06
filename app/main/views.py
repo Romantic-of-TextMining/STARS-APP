21c70c27ddfa00296acfe56147e72c5d2a48f5c1
Huang Chuan <river1440>
2022-05-06 21:10:57 +0800

    textcloud store in assest, and present in index

--- app/main/views.py
+++ app/main/views.py
@@ -4,25 +4,13 @@ from ..models import User
 from ..email import send_email
 from . import main
 from .forms import NameForm
-
+from . import text_cloud
+import os
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    dirname = os.path.dirname(__file__)
+    path = os.path.join(dirname, '../static/assets/data.json')
+    new_text_cloud = text_cloud.AddTextCloud(path)
+    new_text_cloud.create_textcloud()
+    return render_template('index.html')
