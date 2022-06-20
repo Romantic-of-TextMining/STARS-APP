e9adca07d7b51736480fddda4cfac6d812976b32
Huang Chuan <river1440>
2022-06-20 21:27:10 +0800

    fix: heroku issue

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,8 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
+    print(f"config_name:{config_name}\n")
+    print(f"config[config_name]:{config[config_name]}")
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
