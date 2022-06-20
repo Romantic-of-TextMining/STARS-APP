269b0f62292abc4302b45292f200f63f5e733ae9
Huang Chuan <river1440>
2022-06-20 21:34:22 +0800

    fix: put heroku logs

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,8 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
-    app = Flask(__name__)
-    print(f"config_name:{config_name}\n")
     print(f"config[config_name]:{config[config_name]}")
+    app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
